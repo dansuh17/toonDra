c0983079c571192762d5f08c58e712c5216ecf33
harry2636 <harry2636@naver.com>
2016-10-01 23:54:53 +0900

    Implemented dashboard page that has preview mode and gridView mode,             which can be switched using top-right button.
    Show thumbnails and firse epsidoe preview in the main layout with
    network connection.
    
    Made sample pages for listview and read view for demo purpose.

---
+++ Android/toonDra/app/src/main/java/edu/kaist/mskers/toondra/EpisodeListPage.java
@@ -0,0 +1,34 @@
+package edu.kaist.mskers.toondra;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+/**
+ * Created by harrykim on 2016. 10. 14..
+ */
+
+public class EpisodeListPage extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.episodelist_fragment);
+
+    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    getSupportActionBar().setTitle("Sample listview from Naver Webtoon");
+    ImageView imageView = (ImageView) findViewById(R.id.episodeListView);
+
+    imageView.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), ReadEpisodePage.class);
+        startActivity(intent);
+      }
+    });
+  }
+}
--- Android/toonDra/app/src/main/java/edu/kaist/mskers/toondra/MainActivity.java
+++ Android/toonDra/app/src/main/java/edu/kaist/mskers/toondra/MainActivity.java
@@ -1,22 +1,58 @@
 package edu.kaist.mskers.toondra;
 
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.ALL;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.FRI;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.MON;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.SAT;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.SUN;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.THURS;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.TUES;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.Day.WEDS;
+import static edu.kaist.mskers.toondra.navermodule.webtoon.NaverWebtoonCrawler.downloadWebtoonListByDay;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import android.os.SystemClock;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Button;
+
+import edu.kaist.mskers.toondra.navermodule.NaverToonInfo;
+import edu.kaist.mskers.toondra.navermodule.webtoon.Day;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+    implements NavigationView.OnNavigationItemSelectedListener {
+
+  private ArrayList<Map.Entry<Day, Button>> dayButtonArray = new ArrayList<>();
+  private NaverToonInfo[][] webtoonInfoByDay = new NaverToonInfo[7][];
+  private Day currentDay;
+
+  /* 0 -> preViewModeFragment now, 1 -> GridViewModeFragment now */
+  private int currentFragment = 0;
+  private final int previewModeFlag = 0;
+  private final int gridviewModeFlag = 1;
+
+  private PreviewModeFragment previewModeFragment;
+  private GridViewModeFragment gridViewModeFragment;
+
+  private long lastClickTime = 0;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,41 +61,26 @@ public class MainActivity extends AppCompatActivity
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show();
-      }
-    });
-
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-            this, drawer, toolbar, R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close);
+        this, drawer, toolbar, R.string.navigation_drawer_open,
+        R.string.navigation_drawer_close);
     drawer.addDrawerListener(toggle);
     toggle.syncState();
 
     NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
     navigationView.setNavigationItemSelectedListener(this);
 
-    LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout1);
-    linearLayout.setOnClickListener(mClickListener);
-  }
-  private int flag = 1;
-  private final LinearLayout.OnClickListener mClickListener = new View.OnClickListener() {
-    public void onClick(View v) {
-      TextView textFruit = (TextView) findViewById(R.id.title_text1);
-      if ((flag++) % 2 == 1) {
-        textFruit.setText("changed");
-      } else {
-        textFruit.setText("returned");
-      }
-    }
-  };
+    downloadAllWebtoons();
 
-  @Override
+    setRealCurrentDay();
+    setDayButtons();
+
+    initializeFragments();
+  }
+  /**
+   * Catch the back press button press on the screen.
+   */
   public void onBackPressed() {
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     if (drawer.isDrawerOpen(GravityCompat.START)) {
@@ -83,8 +104,8 @@ public class MainActivity extends AppCompatActivity
     // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    if (id == R.id.change_fragment) {
+      changeFragment(item);
       return true;
     }
 
@@ -114,5 +135,250 @@ public class MainActivity extends AppCompatActivity
     DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
     drawer.closeDrawer(GravityCompat.START);
     return true;
+  }
+
+  /**
+   * Get the current day which is selected by the user, default is real-time current day.
+   */
+  public Day getCurrentDay() {
+    return currentDay;
+  }
+
+  /**
+   * Set the current day which is selected by the user, default is real-time current day.
+   */
+  public void setCurrentDay(Day day) {
+    currentDay = day;
+  }
+
+  public ArrayList<Map.Entry<Day, Button>> getDayButtonArray() {
+    return dayButtonArray;
+  }
+
+
+  /**
+   * Change the fragment to either preview mode or gridView mode.
+   * Icon on the top-right will be changed accordingly.
+   */
+  public void changeFragment(MenuItem item) {
+    Log.e("ChangeFragment", "" + currentFragment);
+    Fragment currentFragmentObj;
+    Fragment nextFragmentObj;
+    switch (currentFragment) {
+      default:
+      case previewModeFlag: {
+        currentFragmentObj = previewModeFragment;
+        nextFragmentObj = gridViewModeFragment;
+        currentFragment = gridviewModeFlag;
+        item.setIcon(R.drawable.preview_menu);
+        break;
+      }
+      case gridviewModeFlag: {
+        currentFragmentObj = gridViewModeFragment;
+        nextFragmentObj = previewModeFragment;
+        currentFragment = previewModeFlag;
+        item.setIcon(R.drawable.gridview_menu);
+        break;
+      }
+    }
+
+    FragmentManager fragmentManager = getFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.hide(currentFragmentObj);
+    fragmentTransaction.show(nextFragmentObj);
+    addThumbnails();
+    fragmentTransaction.commit();
+  }
+
+  /**
+   * Get the current webtoon info list of the selected day.
+   */
+  public NaverToonInfo[] getCurrentWebtoonsByDay(Day day) {
+    return webtoonInfoByDay[dayToIndex(day)];
+  }
+
+  /**
+   * Change the Enum day type to corresponding integer index.
+   */
+  public int dayToIndex(Day day) {
+    int index = -1;
+    switch (day) {
+      case MON:
+        index = 0;
+        break;
+      case TUES:
+        index = 1;
+        break;
+      case WEDS:
+        index = 2;
+        break;
+      case THURS:
+        index = 3;
+        break;
+      case FRI:
+        index = 4;
+        break;
+      case SAT:
+        index = 5;
+        break;
+      case SUN:
+        index = 6;
+        break;
+      default:
+        index = 0;
+        break;
+    }
+    return index;
+  }
+
+  /**
+   * Set the current day as the real-time current day.
+   */
+  private void setRealCurrentDay() {
+    Calendar calendar = Calendar.getInstance();
+    int day = calendar.get(Calendar.DAY_OF_WEEK);
+    switch (day) {
+      case Calendar.MONDAY:
+        currentDay = MON;
+        break;
+      case Calendar.TUESDAY:
+        currentDay = TUES;
+        break;
+      case Calendar.WEDNESDAY:
+        currentDay = WEDS;
+        break;
+      case Calendar.THURSDAY:
+        currentDay = THURS;
+        break;
+      case Calendar.FRIDAY:
+        currentDay = FRI;
+        break;
+      case Calendar.SATURDAY:
+        currentDay = SAT;
+        break;
+      case Calendar.SUNDAY:
+        currentDay = SUN;
+        break;
+      default:
+        currentDay = MON;
+        break;
+    }
+  }
+
+  /**
+   * Set the day buttons with click detection.
+   */
+  private void setDayButtons() {
+    Button mon = (Button) findViewById(R.id.button_mon);
+    Button tue = (Button) findViewById(R.id.button_tue);
+    Button wed = (Button) findViewById(R.id.button_wed);
+    Button thu = (Button) findViewById(R.id.button_thu);
+    Button fri = (Button) findViewById(R.id.button_fri);
+    Button sat = (Button) findViewById(R.id.button_sat);
+    Button sun = (Button) findViewById(R.id.button_sun);
+
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(MON, mon));
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(TUES, tue));
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(WEDS, wed));
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(THURS, thu));
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(FRI, fri));
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(SAT, sat));
+    dayButtonArray.add(new AbstractMap.SimpleEntry<Day, Button>(SUN, sun));
+
+    setDayButtonClick();
+  }
+
+  /**
+   * When the day button is clicked, it should give corresponding webtoon list views.
+   */
+  private void setDayButtonClick() {
+    for (final Map.Entry<Day, Button> entry : dayButtonArray) {
+      if (entry.getKey() == currentDay) {
+        entry.getValue().setBackgroundColor(ContextCompat.getColor(getApplicationContext(),
+            R.color.activeButtonColor));
+      }
+      entry.getValue().setOnClickListener(new View.OnClickListener() {
+        public void onClick(View view) {
+          if (!checkElapsedTimeIsValid()) {
+            return;
+          }
+          dayButtonArray.get(dayToIndex(currentDay)).getValue().setBackgroundColor(ContextCompat
+              .getColor(getApplicationContext(), R.color.waitButtonColor));
+          entry.getValue().setBackgroundColor(ContextCompat
+              .getColor(getApplicationContext(), R.color.activeButtonColor));
+          setCurrentDay(entry.getKey());
+          Log.e("current_day", currentDay.toString());
+          addThumbnails();
+        }
+      });
+    }
+  }
+
+  /**
+   * Add thumbnails to the main view, according to the current mode.
+   */
+  private void addThumbnails() {
+    if (currentFragment == previewModeFlag) {
+      previewModeFragment.addThumbnailsToLeftByDay(currentDay);
+    } else if (currentFragment == gridviewModeFlag) {
+      gridViewModeFragment.addThumbnailsToGrid(currentDay);
+    } else {
+      Log.e("flag", "Invalid flag error");
+      finish();
+    }
+  }
+
+  /**
+   * Download all the webtoon information uploaded in the website.
+   */
+  private void downloadAllWebtoons() {
+    for (final Day day : Day.values()) {
+      if (day == ALL) {
+        continue;
+      }
+      Thread thread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+          if (webtoonInfoByDay[dayToIndex(day)] == null) {
+            NaverToonInfo[] webtoonInfo = downloadWebtoonListByDay(day);
+            webtoonInfoByDay[dayToIndex(day)] = webtoonInfo;
+          }
+        }
+      });
+      thread.start();
+
+      try {
+        thread.join();
+      } catch (InterruptedException ex) {
+        ex.printStackTrace();
+      }
+    }
+  }
+
+  /**
+   * Check the elapsed time after the click is occurred.
+   */
+  private boolean checkElapsedTimeIsValid() {
+    // preventing double, using threshold of 300 ms
+    if (SystemClock.elapsedRealtime() - lastClickTime < 300) {
+      return false;
+    }
+    lastClickTime = SystemClock.elapsedRealtime();
+    return true;
+  }
+
+  /**
+   * Initialize the preview and gridView fragments to enable conversion between them.
+   */
+  private void initializeFragments() {
+    previewModeFragment = new PreviewModeFragment();
+    gridViewModeFragment = new GridViewModeFragment();
+
+    FragmentManager fragmentManager = getFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.add(R.id.container, previewModeFragment);
+    fragmentTransaction.add(R.id.container, gridViewModeFragment);
+    fragmentTransaction.hide(gridViewModeFragment);
+    fragmentTransaction.commit();
   }
 }
